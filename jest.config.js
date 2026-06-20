c99fda42676f6aea16467d95ba7e792873394cc5
hjy080530 <bomaye119@gmail.com>
2026-06-20 23:12:52 +0900

    chore(config): 의존성, 스크립트, 프로젝트 설정 추가

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
+const preset = require('@react-native/jest-preset/jest-preset');
+
 module.exports = {
-  preset: '@react-native/jest-preset',
+  ...preset,
+  moduleNameMapper: {
+    'react-native-webview': '<rootDir>/__mocks__/react-native-webview.js',
+  },
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  watchman: false,
 };
